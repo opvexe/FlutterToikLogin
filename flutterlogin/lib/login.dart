6b50c9a3c64c620704233924721a54de5d1c411f
FaceBook <tao_shuminX@foxmail.com>
2019-03-05 09:15:16 +0800

    no message

--- flutterlogin/lib/login.dart
+++ flutterlogin/lib/login.dart
@@ -28,20 +28,30 @@ class loginWidget extends StatelessWidget {
               children: <Widget>[
                 Container(
                   child: Padding(
-                    padding: EdgeInsets.only(left: 20.0),
-                    child: Icon(
-                      Icons.close,
+                    padding: EdgeInsets.only(left: 15.0),
+                    child: IconButton(
+                      padding: EdgeInsets.zero,
+                      iconSize: 25.0,
+                      icon: Icon(Icons.close),
                       color: Colors.white,
-                      size: 25.0,
+                      onPressed: () {
+                        print("close");
+                      },
                     ),
                   ),
                 ),
                 Container(
                   child: Padding(
-                    padding: EdgeInsets.only(right: 20.0),
-                    child: Text(
-                      "密码登录",
-                      style: TextStyle(fontSize: 18.0, color: Colors.white),
+                    padding: EdgeInsets.only(right: 15.0),
+                    child: FlatButton(
+                      child: Text(
+                        "密码登录",
+                        textAlign: TextAlign.right,
+                        style: TextStyle(fontSize: 18.0, color: Colors.white),
+                      ),
+                      onPressed: (){
+                        print("login code ");
+                      },
                     ),
                   ),
                 ),
@@ -63,46 +73,31 @@ class loginWidget extends StatelessWidget {
             margin: const EdgeInsets.only(left: 40.0, right: 40.0, top: 30.0),
             padding: EdgeInsets.only(left: 5.0, right: 5.0),
             alignment: Alignment.center,
+
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.red,
+            ),
+
             child: Row(
               mainAxisAlignment: MainAxisAlignment.start,
               children: <Widget>[
-                FlatButton(
-                  child: Row(
-                    children: <Widget>[
-                      Padding(
-                        padding: EdgeInsets.zero,
-                        child: Text(
-                          "+86",
-                          style: TextStyle(fontSize: 18.0, color: Colors.white),
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(left: 3.0),
-                        child: Icon(
-                          Icons.arrow_drop_down,
-                          size: 20.0,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ],
-                  ),
-                  onPressed: () {
-                    print("+86");
-                  },
-                ),
-                Container(
-                  width: 200.0,
-                  child: TextField(
+                iphoneButton(),
+                Expanded(
+                  child: Container(
+                    child: TextField(
                     textAlign: TextAlign.left,
                     cursorColor: Colors.yellow,
                     keyboardType: TextInputType.number,
                     decoration: InputDecoration(
                       border: InputBorder.none,
                       hintText: "请输入手机号码",
-                      hintStyle: TextStyle(color: Colors.grey.shade200,fontSize: 16.0),
+                      hintStyle: TextStyle(
+                          color: Colors.grey.shade200, fontSize: 16.0),
                     ),
                   ),
-                ),
+                  ),
+                )
               ],
             ),
           ),
@@ -127,16 +122,18 @@ class loginWidget extends StatelessWidget {
                     decoration: InputDecoration(
                       border: InputBorder.none,
                       hintText: "请输入验证码",
-                      hintStyle: TextStyle(color: Colors.grey.shade200,fontSize: 16.0),
+                      hintStyle: TextStyle(
+                          color: Colors.grey.shade200, fontSize: 16.0),
                     ),
                   ),
                 ),
                 Expanded(
                   child: FlatButton(
-                    padding: EdgeInsets.only(left: 5.0,right: 5.0),
+                    padding: EdgeInsets.only(left: 5.0, right: 5.0),
                     child: Text(
                       "获取验证码",
-                      style: TextStyle(fontSize: 16.0, color: Colors.grey.shade200),
+                      style: TextStyle(
+                          fontSize: 16.0, color: Colors.grey.shade200),
                     ),
                   ),
                 ),
@@ -144,11 +141,11 @@ class loginWidget extends StatelessWidget {
             ),
           ),
           Container(
-            margin: EdgeInsets.only(top: 60.0),
+            margin: EdgeInsets.only(top: 80.0),
             alignment: Alignment.center,
             child: Text(
               "其他登录方式",
-              style:TextStyle(fontSize: 15.0,color: Colors.white),
+              style: TextStyle(fontSize: 15.0, color: Colors.white),
             ),
           ),
           Container(
@@ -160,36 +157,78 @@ class loginWidget extends StatelessWidget {
                   padding: EdgeInsets.zero,
                   iconSize: 30.0,
                   icon: Image.asset("assets/images/share_qq.png"),
-                  onPressed: (){
+                  onPressed: () {
                     print("QQ");
                   },
                 ),
-
                 Container(
                   margin: EdgeInsets.only(left: 30.0),
                   child: IconButton(
-                     padding: EdgeInsets.zero,
-                     iconSize: 30.0,
-                  icon: Image.asset("assets/images/share_wechat.png"),
-                  onPressed: (){
-                     print("Wechat");
-                  },
+                    padding: EdgeInsets.zero,
+                    iconSize: 30.0,
+                    icon: Image.asset("assets/images/share_wechat.png"),
+                    onPressed: () {
+                      print("Wechat");
+                    },
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(left: 30.0),
+                  child: IconButton(
+                    padding: EdgeInsets.zero,
+                    iconSize: 30.0,
+                    icon: Image.asset("assets/images/share_wechat.png"),
+                    onPressed: () {
+                      print("Wechat");
+                    },
                   ),
                 ),
               ],
             ),
           ),
-
           Container(
             margin: EdgeInsets.only(top: 20.0),
             alignment: Alignment.center,
             child: Text(
               "登录遇到问题？",
-              style:TextStyle(fontSize: 15.0,color: Colors.yellow),
+              style: TextStyle(fontSize: 15.0, color: Colors.yellow),
             ),
           ),
         ],
       ),
+    );
+  }
+}
+
+
+
+// 获取区号
+class iphoneButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+       child: Row(
+          children: <Widget>[
+              Padding(
+                padding: EdgeInsets.zero,
+                child: Text(
+                  "+86",
+                 style: TextStyle(fontSize: 18.0, color: Colors.white),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 3.0,right: 0.0),
+                child: Icon(
+                  Icons.arrow_drop_down,
+                  size: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+          ],
+       ),
+       onPressed: (){
+         print("+ 86");
+       },
     );
   }
 }
